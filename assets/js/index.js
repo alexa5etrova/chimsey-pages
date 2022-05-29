c1f36139434f51b904a05ce76a4cf4ab69aa665e
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-05-29 21:54:48 +0300

    adaptive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,12 @@
-(() => {
-})();
+(() => {})();
+const burgerBtnHandler = () => {
+  let burgerMenu = document.getElementById('burgerMenu');
+  if (burgerMenu.classList.contains('header--burgerMenu-nonactive')) {
+    burgerMenu.classList.remove('header--burgerMenu-nonactive');
+    burgerMenu.classList.add('header--burgerMenu-active');
+  } else {
+    burgerMenu.classList.remove('header--burgerMenu-active');
+    burgerMenu.classList.add('header--burgerMenu-nonactive');
+  }
+};
 //# sourceMappingURL=index.js.map
