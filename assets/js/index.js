a44bf29e8daec3b6725c135f4ce870d98dfca7ce
Alexandra Petrova <aleksa.5etrova@gmail.com>
2022-06-03 12:38:00 +0300

    localStorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,28 @@
-(() => {})();
+(() => {
+  // src/components/themeChangebtn/themeChangebtn.js
+})();
+//# sourceMappingURL=index.js.map const
+themeEl = document.getElementsByTagName('body')[0];
+if (localStorage.getItem('theme') == 'dark') {
+  themeEl.classList.remove('lightTheme');
+  themeEl.classList.add('darkTheme');
+} else {
+  themeEl.classList.add('lightTheme');
+  themeEl.classList.remove('darkTheme');
+}
+const themeChangeHandler = (event) => {
+  event.preventDefault();
+
+  if (themeEl.classList.contains('lightTheme')) {
+    themeEl.classList.remove('lightTheme');
+    themeEl.classList.add('darkTheme');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    themeEl.classList.remove('darkTheme');
+    themeEl.classList.add('lightTheme');
+    localStorage.setItem('theme', 'light');
+  }
+};
 const burgerBtnHandler = () => {
   let burgerMenu = document.getElementById('burgerMenu');
   if (burgerMenu.classList.contains('header--burgerMenu-nonactive')) {
@@ -9,4 +33,3 @@ const burgerBtnHandler = () => {
     burgerMenu.classList.add('header--burgerMenu-nonactive');
   }
 };
-//# sourceMappingURL=index.js.map
